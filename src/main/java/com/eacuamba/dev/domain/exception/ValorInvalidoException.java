017f34be2a6180dc0d0f281e212d254e0666eac1
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-12-30 21:26:13 +0200

    Classe que representa uma excepção, um valor invalido.

---
+++ src/main/java/com/eacuamba/dev/domain/exception/ValorInvalidoException.java
@@ -0,0 +1,7 @@
+package com.eacuamba.dev.domain.exception;
+
+public class ValorInvalidoException extends Exception{
+    public  ValorInvalidoException(String message){
+       super(message);
+    }
+}
