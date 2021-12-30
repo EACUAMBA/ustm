d7a7a209e8057acaa8acdcc4fba95d9214669e1c
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-12-30 21:30:40 +0200

    Classe que simula uma classe de conexão com banco de dados e etc.

---
+++ src/main/java/com/eacuamba/dev/domain/repository/LocalizacaoRepositoryFAKE.java
@@ -0,0 +1,60 @@
+package com.eacuamba.dev.domain.repository;
+
+import com.eacuamba.dev.domain.exception.ValorInvalidoException;
+import com.eacuamba.dev.domain.model.Localizacao;
+import com.eacuamba.dev.domain.validator.LocalizacaoValidator;
+
+import java.util.*;
+
+public class LocalizacaoRepositoryFAKE {
+    private static LocalizacaoRepositoryFAKE localizacaoRepositoryFAKE;
+
+    private List<Localizacao> localizacaoList = new ArrayList<>();
+
+     {
+        try {
+            Localizacao triunfo = new Localizacao();
+            triunfo.setId(proximoId());
+            triunfo.setDesignacao("Triunfo");
+            save(triunfo);
+
+            Localizacao sommerchield = new Localizacao();
+            sommerchield.setId(proximoId());
+            sommerchield.setDesignacao("Sommerchield");
+            save(sommerchield);
+        }catch (ValorInvalidoException valorInvalidoException){
+            System.err.println(valorInvalidoException.getMessage());
+            System.err.flush();
+            System.err.close();
+        }
+    }
+
+    private Long proximoId(){
+        localizacaoList.sort(Comparator.comparing(Localizacao::getId));
+        return localizacaoList.size() > 0 ? Math.addExact(localizacaoList.get(localizacaoList.size()-1).getId(), 1) : 1;
+    }
+
+    public List<Localizacao> findAll(){
+        return localizacaoList;
+    }
+
+    public Optional<Localizacao> findById(Long id){
+        return localizacaoList.stream().filter((l-> l.getId().equals(id))).findFirst();
+    }
+
+    public Optional<Localizacao> save(Localizacao localizacao) throws ValorInvalidoException {
+        localizacao.setId(proximoId());
+
+        LocalizacaoValidator.valida(localizacao);
+
+        localizacaoList.add(localizacao);
+        return Optional.of(localizacao);
+    }
+
+    public static LocalizacaoRepositoryFAKE getInstance(){
+        if(localizacaoRepositoryFAKE == null){
+            localizacaoRepositoryFAKE = new LocalizacaoRepositoryFAKE();
+        }
+        return localizacaoRepositoryFAKE;
+    }
+}
---
+++ src/main/java/com/eacuamba/dev/domain/repository/PropriedadeRepositoryFAKE.java
@@ -0,0 +1,45 @@
+package com.eacuamba.dev.domain.repository;
+
+import com.eacuamba.dev.domain.exception.ValorInvalidoException;
+import com.eacuamba.dev.domain.model.Localizacao;
+import com.eacuamba.dev.domain.model.Propriedade;
+import com.eacuamba.dev.domain.model.TipoPropriedade;
+import com.eacuamba.dev.domain.validator.PropriedadeValidator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class PropriedadeRepositoryFAKE {
+    private static  PropriedadeRepositoryFAKE propriedadeRepositoryFAKE;
+    private static List<Propriedade> propriedadeList = new ArrayList<>();
+
+    private  Long proximoId(){
+        propriedadeList.sort(Comparator.comparing(Propriedade::getId));
+        return propriedadeList.size() > 0 ? Math.addExact(propriedadeList.get(propriedadeList.size()-1).getId(), 1) : 1;
+    }
+
+    public  List<Propriedade> findAll(){
+        return propriedadeList;
+    }
+
+    public  Optional<Propriedade> findById(Long id){
+        return propriedadeList.stream().filter((l-> l.getId().equals(id))).findFirst();
+    }
+
+    public Optional<Propriedade> save(Propriedade propriedade) throws ValorInvalidoException {
+        propriedade.setId(proximoId());
+        propriedadeList.add(propriedade);
+        return Optional.of(propriedade);
+    }
+
+    public static PropriedadeRepositoryFAKE getInstance(){
+        if(propriedadeRepositoryFAKE == null){
+            propriedadeRepositoryFAKE = new PropriedadeRepositoryFAKE();
+        }
+        return propriedadeRepositoryFAKE;
+    }
+
+
+}
