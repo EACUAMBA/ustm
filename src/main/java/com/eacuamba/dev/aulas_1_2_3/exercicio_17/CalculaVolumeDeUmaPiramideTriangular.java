a894a89355645d21217a3a9849d0e808aa614e00
Edilson Alexandre Cuamba <edilson.cuamba@exi.co.mz>
2022-03-01 12:30:48 +0200

    Exercicio 17, Classe que calcula o volume da pirâmide.

---
+++ src/main/java/com/eacuamba/dev/aulas_1_2_3/exercicio_17/CalculaVolumeDeUmaPiramideTriangular.java
@@ -0,0 +1,15 @@
+package com.eacuamba.dev.aulas_1_2_3.exercicio_17;
+
+import java.math.BigDecimal;
+
+import com.eacuamba.dev.utils.ConsoleBase;
+
+public class CalculaVolumeDeUmaPiramideTriangular extends ConsoleBase{
+	public static void main(String[] args) {
+		output.println("Calcula o volume de uma pirâmide.");
+		BigDecimal areaBase = getValorNumericoAsBigDecimalFromConsole("Area da base");
+		BigDecimal altura = getValorNumericoAsBigDecimalFromConsole("Altura");
+		BigDecimal volume = areaBase.multiply(altura).divide(BigDecimal.valueOf(3));
+		output.printf("O volume de uma pirâmide com area base %.2f e altura %.2f é de %.2f.%n", areaBase, altura, volume);
+	}
+}
