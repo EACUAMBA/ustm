3a8e581d1d6492164cb9647862ec1b757afe57f6
Edilson Alexandre Cuamba <edilson.cuamba@exi.co.mz>
2022-04-25 13:35:28 +0200

    Exercicios para preparação da primeira avaliacao de programação 2.
    exercicios do teste USTM-POO-2021-SemI-TesteII-V1.docx

--- src/main/java/com/eacuamba/dev/iii_semestre/teste_1/ustm_poo_2021_semi_teste_ii_v1/docx/exercicio_8/App.java
+++ src/main/java/com/eacuamba/dev/iii_semestre/teste_1/ustm_poo_2021_semi_teste_ii_v1/docx/exercicio_8/App.java
@@ -1,4 +1,4 @@
-package com.eacuamba.dev.iii_semestre.teste_1.exercicio_8;
+package com.eacuamba.dev.iii_semestre.teste_1.ustm_poo_2021_semi_teste_ii_v1.docx.exercicio_8;
 
 public class App {
   public static void main(String[] args) {
--- src/main/java/com/eacuamba/dev/iii_semestre/teste_1/ustm_poo_2021_semi_teste_ii_v1/docx/exercicio_9/VIP.java
+++ src/main/java/com/eacuamba/dev/iii_semestre/teste_1/ustm_poo_2021_semi_teste_ii_v1/docx/exercicio_9/VIP.java
@@ -1,4 +1,4 @@
-package com.eacuamba.dev.iii_semestre.teste_1.exercicio_9;
+package com.eacuamba.dev.iii_semestre.teste_1.ustm_poo_2021_semi_teste_ii_v1.docx.exercicio_9;
 
 public class VIP extends Ingresso{
     //teste: USTM-POO-2021-SemI-TesteII-V1.docx
