3c3e9ae830fb4a8a49868a89bb681be60f0cca75
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-05-03 21:50:59 +0200

    Aprendendo Applet.

---
+++ src/main/java/com/eacuamba/dev/iii_semestre/aula_x/BolaSaltante.java
@@ -0,0 +1,13 @@
+package com.eacuamba.dev.iii_semestre.aula_x;
+
+import java.applet.Applet;
+
+public class BolaSaltante extends Applet implements Runnable{
+
+    private Integer x = 150, y = 50, r = 10;
+
+    @Override
+    public void run() {
+
+    }
+}
