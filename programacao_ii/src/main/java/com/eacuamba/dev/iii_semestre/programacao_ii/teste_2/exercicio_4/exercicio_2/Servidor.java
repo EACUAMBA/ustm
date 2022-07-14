f82253a43860308596c1fa396e6388a7aeda52e9
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-14 18:07:39 +0200

    Adicionando o projecto.

---
+++ programacao_ii/src/main/java/com/eacuamba/dev/iii_semestre/programacao_ii/teste_2/exercicio_4/exercicio_2/Servidor.java
@@ -0,0 +1,36 @@
+package com.eacuamba.dev.iii_semestre.programacao_ii.teste_2.exercicio_4.exercicio_2;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class Servidor {
+    private static final int serverPort = 8080;
+
+    public static void main(String[] args) {
+        try {
+            byte[] messageByte = new byte[60000];
+            DatagramPacket datagramPacket = new DatagramPacket(messageByte, messageByte.length);
+            DatagramSocket datagramSocket = new DatagramSocket(serverPort);
+            datagramSocket.receive(datagramPacket);
+
+            String s = new String(messageByte);
+            StringBuilder stringBuilder = new StringBuilder();
+            for(char c : s.toCharArray()){
+                if(Character.isUpperCase(c)){
+                    stringBuilder.append(Character.toLowerCase(c));
+                }else
+                    stringBuilder.append(Character.toUpperCase(c));
+            }
+            datagramPacket.setData(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+            datagramSocket.send(datagramPacket);
+
+        } catch (UnknownHostException e) {
+            System.out.println("Ocorreu um erro ao criar o endereço do servidor.");
+        } catch (SocketException e) {
+            System.out.println("Ocorreu um erro ao criar o socket do cliente.");
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao enviar o pacote.");
+        }
+    }
+}
