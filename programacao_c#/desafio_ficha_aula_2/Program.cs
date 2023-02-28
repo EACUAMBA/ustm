c2a7d641005520f976066e7317b6c8304cbfcbc3
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-02-28 13:57:46 +0200

    Desafios da ficha aula2 de c#

---
+++ programacao_c#/desafio_ficha_aula_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program{
+  static void Main(string[] args){
+    LerTresPalavrasSeparadasPorEspaco();
+  }
+
+  static void LerUmTextoAteQuebraDeLinha(){
+    Console.WriteLine("Escreva o texto:");
+    string texto = Console.ReadLine();
+    Console.WriteLine($"O texto escrito foi '{texto}'");
+  }
+
+  static void LerTresPalavrasUmaCadaLinha(){
+    Console.WriteLine("Escreva as palavras:");
+      string texto1 = Console.ReadLine();
+      string texto2 = Console.ReadLine();
+      string texto3 = Console.ReadLine();
+  
+    Console.WriteLine($"As palavras escritas foram '{texto1}'");
+    Console.WriteLine($"As palavras escritas foram '{texto2}'");
+    Console.WriteLine($"As palavras escritas foram '{texto3}'");
+  }
+
+  static void LerTresPalavrasSeparadasPorEspaco(){
+    Console.WriteLine("Escreva as palavras Separadas pode espaços:");
+
+    
+    string texto = Console.ReadLine();
+    string[] palavras = texto.Split(" ");
+    
+    foreach (string palavra in palavras)
+    {
+    Console.WriteLine($"As palavras escritas foram '{palavra}'");
+    }
+  }
+}
