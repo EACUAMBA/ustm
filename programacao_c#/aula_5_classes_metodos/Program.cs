c43c634cd9ec934c6e9bc6e01918e0b0dc36db68
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-03-14 12:32:21 +0200

    Implementando alguns programas em c#

---
+++ programacao_c#/aula_5_classes_metodos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula5ClassesMetodos
+{
+    class LerDadosDeDuasPessoasDepoisMostraMaisVelha
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoaMaisVelha = null;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Dados da pessoa {i + 1}");
+                Pessoa pessoa = new Pessoa();
+
+                Console.Write("Nome: ");
+                pessoa.Nome = Console.ReadLine();
+
+                Console.Write("Idade: ");
+                pessoa.Idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (pessoaMaisVelha != null)
+                {
+                    if (pessoa.Idade > pessoaMaisVelha.Idade)
+                        pessoaMaisVelha = pessoa;
+                }
+                else
+                {
+                    pessoaMaisVelha = pessoa;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Pessoa mais velha: {pessoaMaisVelha}");
+        }
+
+        class Pessoa
+        {
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+
+            override
+            public string ToString()
+            {
+                return Nome;
+            }
+        }
+    }
+}
---
+++ programacao_c#/miniteste_20230310/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace MiniTest
+{
+    class MiniTest
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"O valor é {20.4/40.5}");
+        }
+    }
+}
