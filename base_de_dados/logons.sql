aec4efd1ad88c908c3f8fc23f6eb9c22898940c4
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-05-07 21:40:50 +0200

    Criando logons e associando a uma base de dados e dando permissões.

---
+++ base_de_dados/logons.sql
@@ -0,0 +1,26 @@
+
+-- Criando um logon para a minha instancia SQLServer, utilizando a autenticação do windows.
+-- Funcionaria se a minha instancia não estivesse em um container linux
+CREATE LOGIN [EACUAMBA-HP\eacuamba]
+    FROM WINDOWS
+WITH DEFAULT_DATABASE = [CAR_WASH];
+GO;
+
+CREATE LOGIN basededados
+    WITH PASSWORD = 'B4s3.d3.d4d0$';
+;
+GO;
+
+
+-- Criando um utilizador associado ao logon  [EACUAMBA-HP\eacuamba] para utilizar a base de dados
+use car_wash;
+go;
+
+create user [basededados] for login [basededados];
+go;
+
+-- garantindo permissão de fazer consultas a tabela especifica
+Grant select on automovel to [basededados];
+go;
+
+-- Incrivel fogo!
