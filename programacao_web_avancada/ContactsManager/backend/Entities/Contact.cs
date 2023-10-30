8e40444c6a04989b9da4051f65c839cb5f748cec
Edilson Alexandre Cuamba <edilsonacuamba@outlook.com>
2023-10-30 19:36:54 +0200

    Improved contact entity.

--- programacao_web_avancada/ContactsManager/backend/Entities/Contact.cs
+++ programacao_web_avancada/ContactsManager/backend/Entities/Contact.cs
@@ -10,6 +10,6 @@ public class Contact
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int? Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Phone { get; set; } = string.Empty;
+    public string? Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
 }
