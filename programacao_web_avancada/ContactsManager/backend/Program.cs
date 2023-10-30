d5b20b616d97a66b5a8530553d108bf3b98012bd
Edilson Alexandre Cuamba <edilsonacuamba@outlook.com>
2023-10-30 14:17:58 +0200

    Added jwt configuration.

--- programacao_web_avancada/ContactsManager/backend/Program.cs
+++ programacao_web_avancada/ContactsManager/backend/Program.cs
@@ -1,4 +1,28 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
+
+//Getting values from appsettings.json file
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+var jwtKeyAsUTF8Bytes = Encoding.UTF8.GetBytes(jwtKey);
+var jwtKeyAsSymmetricSecurityKey = new SymmetricSecurityKey(jwtKeyAsUTF8Bytes);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer((options) => {
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters{
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtIssuer,
+        IssuerSigningKey = jwtKeyAsSymmetricSecurityKey
+    };
+});
+
+
 builder.Services.AddControllers();
 var app = builder.Build();
 app.MapControllers();
