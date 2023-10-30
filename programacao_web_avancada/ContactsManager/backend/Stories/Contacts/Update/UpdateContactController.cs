d40730e785b1d1cc50a92486b0dbc63c6dd55b39
Edilson Alexandre Cuamba <edilsonacuamba@outlook.com>
2023-10-30 19:37:34 +0200

    Implemented contact update.

---
+++ programacao_web_avancada/ContactsManager/backend/Stories/Contacts/Update/UpdateContactController.cs
@@ -0,0 +1,26 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Stories.Contacts.Update;
+
+[Authorize]
+[ApiController]
+[Route("api/contacts/{id:int}")]
+public class UpdateContactController : ControllerBase
+{
+
+    private readonly UpdateContactService _updateContactService;
+
+    public UpdateContactController(UpdateContactService updateContactService){
+        this._updateContactService = updateContactService;
+    }
+
+    [HttpPut]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public ActionResult Update(Int32 id, UpdateContactRequest updateContactRequest)
+    {
+        return StatusCode(200, this._updateContactService.Update(id, updateContactRequest));
+    }
+}
---
+++ programacao_web_avancada/ContactsManager/backend/Stories/Contacts/Update/UpdateContactService.cs
@@ -0,0 +1,49 @@
+using DbContexts;
+using Entities;
+using Microsoft.IdentityModel.Tokens;
+using Middlewares;
+
+namespace backend.Stories.Contacts.Update;
+
+public class UpdateContactService
+{
+    private readonly SqliteDbContext _sqliteDbContext;
+
+    public UpdateContactService(SqliteDbContext sqliteDbContext)
+    {
+        this._sqliteDbContext = sqliteDbContext;
+    }
+
+    public int? Update(Int32 id, UpdateContactRequest updateContactRequest)
+    {
+        List<Contact> contactListWithSameId = this._sqliteDbContext.Contacts.Where(c => c.Id.Equals(id)).ToList();
+
+        if (contactListWithSameId.IsNullOrEmpty())
+        {
+            throw new ContactsManagerException(404, $"Contacto com o id {id} não foi encontrado!");
+        }
+        Contact contactToUpdate = contactListWithSameId.First();
+
+        
+        List<Contact> contactListWithSamePhoneNumber = this._sqliteDbContext.Contacts
+            .Where(c => c.PhoneNumber.Equals(updateContactRequest.PhoneNumber)).ToList();
+        if (!contactListWithSamePhoneNumber.IsNullOrEmpty())
+        {
+            Contact contactWithSamePhoneNumber = contactListWithSamePhoneNumber.First();
+            if (!contactWithSamePhoneNumber.Id.Equals(id))
+            {
+                throw new ContactsManagerException(409,
+                    $"Contacto com o número ({contactWithSamePhoneNumber.PhoneNumber}) já existe, é do ({contactWithSamePhoneNumber.Name}) com email ({contactWithSamePhoneNumber.Email}).");
+            }
+        }
+
+        contactToUpdate.Name = updateContactRequest.Name;
+        contactToUpdate.Email = updateContactRequest.Email;
+        contactToUpdate.PhoneNumber = updateContactRequest.PhoneNumber;
+
+        this._sqliteDbContext.Contacts.Update(contactToUpdate);
+        this._sqliteDbContext.SaveChanges();
+
+        return contactToUpdate.Id;
+    }
+}
