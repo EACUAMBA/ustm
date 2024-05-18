7cd78b0943ba4cc668555653d304edb69fe7e4c4
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2024-05-18 10:11:34 +0200

    implemented firebase auth, followed the paper Aula 7 - Persitencia de dados (Firebase).pdf by Ataíde G.P. Chilaúle #2

--- aplicativos_moveis/firebase_get_started/lib/main.dart
+++ aplicativos_moveis/firebase_get_started/lib/main.dart
@@ -1,24 +1,16 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_get_started/screens/authentication_screen.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  FirebaseUIAuth.configureProviders(
-    [
-      EmailAuthProvider()
-    ]
-  );
+  FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
 
   runApp(const MyApp());
 }
